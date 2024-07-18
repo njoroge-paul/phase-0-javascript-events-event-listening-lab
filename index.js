293507cd384185ea19b4f2a08d534256b4454d1c
Duncan <njorogedun870@gmail.com>
2024-07-18 12:54:31 +0300

    made changes

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
-}
+    const button = document.getElementById('button');
+    button.addEventListener('click', function() {
+      console.log('Button clicked!');
+    });
+  }
+  
+  module.exports = addingEventListener;
